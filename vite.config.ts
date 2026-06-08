0c0122c2511fef24eacb148ff2cab5aa20269136
Heru Trijaya <elniteru@gmail.com>
2026-06-08 10:45:39 +0800

    feat: add github actions and source code

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/',
+  base: '/',
 })
